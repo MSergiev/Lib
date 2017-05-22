68a13b30f1bfd9d9d5e93ab63c410b34856a84f1
Miroslav Sergiev <sergievmiroslav@gmail.com>
2017-05-22 21:17:38 +0300

    Separated currencyFormatter and printMatrix into separate files

--- PrintMatrix.h
+++ PrintMatrix.h
@@ -1,58 +1,13 @@
 //EGT required functions library
 
-#ifndef LIB_H
-#define LIB_H
+#ifndef PRINTMATRIX_H
+#define PRINTMATRIX_H
 
 #include <iostream>
-#include <sstream>
-#include <deque>
-#include <algorithm>
-#include <cmath>
-using std::string;
-using std::stringstream;
-using std::endl;
-using std::deque;
-using std::reverse;
 using std::cout;
 using std::endl;
 
-//Currency formatter function
-string currencyFormatter(double value, const char symbol){
-	//Handle decimal part
-	stringstream str;
-	int dec = value;
-	value-=dec;
-	deque<unsigned> digits;
-	while(dec!=0){
-		digits.push_back(dec%10);
-		dec/=10;
-	}
-	int count=0;
-	str << symbol;
-	//Position formatting commas
-	stringstream tmp;
-	while(!digits.empty()){
-		tmp << digits.front();
-		digits.pop_front();
-		count++;
-		if(count==3){
-		tmp << ",";
-		count = 0;
-		}
-	}
-	//Reverse string
-	string rev = tmp.str();
-	reverse(rev.begin(), rev.end());
-	str << rev;
 
-	//Handle decimal part
-	value*=100;
-	value = round(value);
-	dec = value;
-	if(value!=0) str << "." << dec;
-	str << endl;
-	return str.str();
-}
 
 //Array printing function
 template <class T>

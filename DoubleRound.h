461c2b8b00d96df6b4846254f9a5a46ea06e6cc0
Miroslav Sergiev <sergievmiroslav@gmail.com>
2017-05-22 22:36:52 +0300

    Added double rounding function

---
+++ DoubleRound.h
@@ -0,0 +1,32 @@
+#ifndef DOUBLEROUND_H
+#define DOUBLEROUND_H
+
+#include <cmath>
+#include <vector>
+using std::vector;
+
+//Double rounding function
+double doubleRound(double num, 
+		unsigned before = 2, unsigned after = 2){
+//Handle whole part
+	unsigned dec = num;
+	num-=dec;
+	vector<unsigned> digits;
+	while(dec!=0){
+		digits.push_back(dec%10);
+		dec/=10;
+	}
+	before=digits.size()-before;
+	unsigned decRight=0;
+	for(unsigned i = 0; i < digits.size(); ++i)
+		(i<before?decRight:dec)+=digits[i]*pow(10,i);
+
+	decRight = (round(decRight/pow(10,before))*pow(10,before));
+	dec+=decRight;
+//Handle fraction part
+	unsigned frac = round(num*pow(10,after));
+	num = dec+(frac/pow(10, after));
+	return num;
+}
+
+#endif

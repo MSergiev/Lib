7e35030adf619e20096c8f2c7bbb0cfa9eb5be26
Miroslav Sergiev <sergievmiroslav@gmail.com>
2017-05-22 22:40:27 +0300

    Updated main and makefile

--- main.cpp
+++ main.cpp
@@ -3,15 +3,24 @@
 #include <iostream>
 #include "CurrencyFormatter.h"
 #include "PrintMatrix.h"
+#include "DoubleRound.h"
 using namespace std;
 
 int main(){
-	cout << "Currency formatter test: ";
-	cout << currencyFormatter(12345.98765, '$');
+
+	double curr = 12345.98765;
+	cout.precision(5);
+	cout << "- Currency formatter test: " << endl;
+	cout << fixed << curr << " -> " << currencyFormatter(curr, '$');
+
 	int arr[12];
 	for (int i = 0; i < 12; i++) arr[i]=(i+1);
-	cout << "Matrix print test: " << endl;
+	cout << "\n- Matrix print test: " << endl;
 	printMatrix(arr, 4, 3);
+	
+	double doub = 98765.98765; 
+	cout << "\n- Double round test: " << endl;
+	cout <<  doub << " -> " << doubleRound(doub) << endl;
 	
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
-run: main.cpp CurrencyFormatter.h PrintMatrix.h
-	g++ -o run main.cpp CurrencyFormatter.h PrintMatrix.h
+run: main.cpp CurrencyFormatter.h PrintMatrix.h DoubleRound.h
+	g++ -o run main.cpp CurrencyFormatter.h PrintMatrix.h DoubleRound.h

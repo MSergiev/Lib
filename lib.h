d7c6a443e2cf243ead58d2a687072722b0f05715
Miroslav Sergiev <sergievmiroslav@gmail.com>
2017-05-22 20:39:46 +0300

    Added matrix printing function

--- lib.h
+++ lib.h
@@ -3,6 +3,7 @@
 #ifndef LIB_H
 #define LIB_H
 
+#include <iostream>
 #include <sstream>
 #include <deque>
 #include <algorithm>
@@ -12,6 +13,8 @@ using std::stringstream;
 using std::endl;
 using std::deque;
 using std::reverse;
+using std::cout;
+using std::endl;
 
 //Currency formatter function
 string currencyFormatter(double value, const char symbol){
@@ -49,6 +52,57 @@ string currencyFormatter(double value, const char symbol){
 	if(value!=0) str << "." << dec;
 	str << endl;
 	return str.str();
+}
+
+//Array printing function
+template <class T>
+void printMatrix(T* arr, 
+		const unsigned& cols=0,
+		const unsigned& rows=1,
+		const unsigned& spaces=1){
+
+//Get max number of digits for spacing
+	T* tmpPtr = arr;
+	T largest = *arr, smallest = *arr;
+	for (unsigned i = 0; i < rows*cols-1; i++){
+		tmpPtr++;
+		if(*tmpPtr > largest) largest = *tmpPtr;
+		if(*tmpPtr < smallest) smallest = *tmpPtr;
+	}
+	
+	unsigned countSmallest=(smallest<0?1:0), 
+			 countLargest=0;
+	smallest = abs(smallest);
+	while(smallest!=0){
+		countSmallest++;
+		smallest/=10;
+	}
+	while(largest!=0){
+		countLargest++;
+		largest/=10;
+	}
+	unsigned spaceMod = (countSmallest>countLargest?countSmallest:countLargest);
+
+//Formatter
+	for(unsigned i = 0; i < rows; ++i){
+		for(unsigned j = 0; j < cols; j++){
+			T tmp = *arr;
+			unsigned digitCount = (tmp<0?1:0);
+			while(tmp!=0){
+				digitCount++;
+				tmp/=10;
+			}
+			cout << *arr;
+			arr++;
+			if(j!=cols-1)
+				for(unsigned k = 0; k < spaceMod-digitCount+spaces; k++)
+					cout << " ";
+		}
+		if(i!=rows-1)
+			for(unsigned k = 0; k < spaces+1; k++)
+				cout << endl;
+	}
+	cout << endl;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ using namespace std;
 int main(){
 	cout << "Currency formatter test: ";
 	cout << currencyFormatter(12345.98765, '$');
+	int arr[12];
+	for (int i = 0; i < 12; i++) arr[i]=(i+1);
+	cout << "Matrix print test: " << endl;
+	printMatrix(arr, 4, 3);
+	
 	return 0;
 }

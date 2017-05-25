b8af021aee45246af63c7b6a5d2f256346847030
Miroslav Sergiev <sergievmiroslav@gmail.com>
2017-05-25 14:43:50 +0300

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 //Test file
 
 #include <iostream>
+#include <vector>
+#include <queue>
 #include "CurrencyFormatter.h"
 #include "PrintMatrix.h"
 #include "DoubleRound.h"
+#include "RandomOperations.h"
 using namespace std;
 
 int main(){
@@ -18,9 +21,33 @@ int main(){
 	cout << "\n- Matrix print test: " << endl;
 	printMatrix(arr, 4, 3);
 	
-	double doub = 98765.98765; 
+	double doub = 1446.65443; 
 	cout << "\n- Double round test: " << endl;
 	cout <<  doub << " -> " << doubleRound(doub) << endl;
 	
+	cout << "\n- Random operations test: " << endl;
+	cout << "Random number from 1 to 100: " << rangeSelect<int>(1, 100) << endl;
+	cout << "Random double: " << randDouble() << endl;
+	vector<char> vec;
+	for(char i = 65; i <= 90; i++) vec.push_back(i);
+	cout << "Random char from vector: " << vecSelector(vec) << endl;
+	cout << "Initial array: " << endl;
+	for (unsigned i = 0; i < vec.size(); i++) cout << vec[i];
+	cout << "\nShuffled array: " << endl;
+	vecShuffle(vec);
+	for (unsigned i = 0; i < vec.size(); i++) cout << vec[i];
+	queue<char> que;
+	for (unsigned i = 0; i < vec.size(); i++) que.push(vec.back());
+	cout << "\nInitial queue: " << endl;
+	queue<char> tmpQue = que;
+	while(tmpQue.size() > 0){ cout << tmpQue.front(); tmpQue.pop();}
+	randomQueue(que);
+	tmpQue = que;
+	cout << "\nRandomized queue: " << endl;
+	while(tmpQue.size() > 0){ cout << tmpQue.front(); tmpQue.pop();}
+	cout << endl;
+
+	
+
 	return 0;
 }
